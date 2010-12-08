ac9af4ef4a2a699d4b587b29a66935428c08c0a1
James Vasile <james@hackervisions.org>
2010-12-08 14:26:44 -0500

    Split off user-functions and made stub for user-defined functions

---
+++ user-designed-funcs.lisp
@@ -0,0 +1,8 @@
+;;;; This is a file stub.  If you are going to add functions to be
+;;;; called from the ledger data files, this is a good place to do it,
+;;;; as the contents of this file will be loaded when you load
+;;;; Sassetti.
+;;;;
+;;;; You might want to tell git to ignore this file.
+
+(in-package #:sassetti)
