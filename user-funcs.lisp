6c7dbdf85497ddb2676f6db6e863860ee55e0a27
James Vasile <james@hackervisions.org>
2010-12-09 19:49:10 -0500

    converted most user-funcs to use parse-entry instead of returning strings

--- user-funcs.lisp
+++ user-funcs.lisp
@@ -5,14 +5,13 @@
 ;;;; See COPYING for copyright and licensing information.
 (in-package #:sassetti)
 
-;; Functions called from the ledger file
 (defun round-cent (f)
   "Round to the nearest hundreth"
   (/ (round f 0.01) 100))
 
 (defun depreciate-recurse (year month category amount term-left)
   "Do the recursive portion of monthly depreciation"
-  (cat (format nil "~a/~a/15 Depreciate ~a~%   Assets:Prepaid:~a            $-~a~%   Expenses:Depreciation:~a     $~a~%~%" 
+  (cat (format nil "~a/~a/15 Depreciate ~a~%   Assets:Prepaid:~a            $-~f~%   Expenses:Depreciation:~a     $~f~%~%" 
 	       year month category
 	       category amount
 	       category amount)
@@ -28,32 +27,41 @@
   (let* ((amount (round-cent (/ total (float term))))
 	(rounding-error (round-cent (- total (* amount term)))))
     (cat (depreciate-recurse year month category amount term)
-	 (unless (equal rounding-error 0.0)
-	   (format nil "~%~a/~a/15 Depreciation Rounding Fix~%   Assets:Prepaid:~a            $~a~%   Expenses:Depreciation:~a     $~a"
-		   (round (/ (+ (* year 12) term) 12.0))   (+ (mod (+ month (- term 1)) 12) 1)
-		   category (* -1 rounding-error)
-		   category rounding-error
-		   )))))
+	 (if (= 0 rounding-error)
+	     ""
+	     (unless (equal rounding-error 0.0)
+	       (format nil "~%~a/~a/15 Depreciation Rounding Fix~%   Assets:Prepaid:~a            $~f~%   Expenses:Depreciation:~a     $~f"
+		       (round (/ (+ (* year 12) term) 12.0))   (+ (mod (+ month (- term 1)) 12) 1)
+		       category (* -1 rounding-error)
+		       category rounding-error
+		       ))))))
 
 (defun unbillable (date description category amount)
   "Charge off unbillable work"
-  (format nil "~a Charge off unbillable ~a~%   Expenses:Unbillable:~a     $~a~%   * Assets:Unbilled:~a   $-~a"
-	  date description category amount category amount))
+  (parse-entry (format nil 
+		       "~a Charge off unbillable ~a~%   Expenses:Unbillable:~a     $~a~%   * Assets:Unbilled:~a   $-~a"
+		       date description category amount category amount)))
 
 (defun shared-income (datedescrip category amount)
   "Income shared equally among partners, paid into the checking account"
-  (format nil "~a~%   Income:~a   $-~a~%   Assets:Checking:James    $~a~%   Assets:Checking:Karl    $~a"
-	  datedescrip category amount (/ amount 2) (/ amount 2)))
-
-(defun shared-expense (datedescrip category amount)
+  (parse-entry (format nil 
+		       "~a~%   Income:~a   $-~a~%   Assets:Checking:James    $~a~%   Assets:Checking:Karl    $~a"
+		       datedescrip category amount (/ amount 2) (/ amount 2))))
+;(preprocess-ledger-file *ledger-fname*)
+(defun shared-expense (datedescrip category amount &key (cleared-p nil) (pending-p nil) (code ""))
   "Expenses shared equally among the partners, paid from the checking account"
-  (format nil "~a~%   Expenses:~a   $~a~%   Assets:Checking:James    $-~a~%   Assets:Checking:Karl    $-~a"
-	  datedescrip category amount (/ amount 2) (/ amount 2)))
+  (parse-entry (format nil 
+		       "~a~%   Expenses:~a   $~a~%   Assets:Checking:James    $-~a~%   Assets:Checking:Karl    $-~a"
+		       datedescrip category amount (/ amount 2) (/ amount 2))))
 
 (defun prepaid-hours (datedescrip partner client amount)
-  (format nil "~a~%   Liabilities:~a:~a          $~a~%   Assets:Checking:~a:Unearned      $~a"
-	  datedescrip partner client amount partner amount))
+  (parse-entry (format nil 
+		       "~a~%   Liabilities:~a:~a          $~a~%   Assets:Checking:~a:Unearned      $~a"
+		       datedescrip partner client amount partner amount)))
 
 (defun satisfied-invoice (date invoice-number partner client amount)
-  (format nil "~a Satisfaction of Invoice #~a~%   * Assets:Receivable:~a:~a        $~a~%   Assets:Checking:~a                       $~a"
-	  date invoice-number partner client (* -1 amount) partner amount))
+  (parse-entry (format nil 
+		       (cat "~a Satisfaction of Invoice #~a~%"
+				"   * Assets:Receivable:~a:~a        $~a~%"
+				"   Assets:Checking:~a        $~a")
+		       date invoice-number partner client (* -1 amount) partner amount)))
