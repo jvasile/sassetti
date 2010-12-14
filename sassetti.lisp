348e2037328352a7c675285f586661cb6acc9736
James Vasile <james@hackervisions.org>
2010-12-14 18:18:55 -0500

    Remove old todo item (handle transactions) in get-as-list

--- sassetti.lisp
+++ sassetti.lisp
@@ -132,7 +132,6 @@
    ))
 
 (defmethod get-as-list ((self entry))
-  "TODO: handle transactions"
   (list (string-form (date self))
 	(if (effective-date self) (string-form (effective-date self)) nil)
 	(desc self)
